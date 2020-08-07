c50746c7909b59bf7fefae55f69ec37be8df91b6
Hari Krishnan <62686776+thefitbea@users.noreply.github.com>
2020-08-07 15:59:57 +0530

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,12 @@
 // alert("JAVASCRIPT loaded!");
+// alert("JAVASCRIPT loaded!");
+
+
+var i,j = document.querySelectorAll(".drum").length;
+
+for(i = 0; i < j; i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var sound = new Audio("sounds/tom-1.mp3");
+    sound.play();
+  })
+}
