66a7605ed4599c607b0276a29e38008b04033e81
Hari Krishnan <62686776+thefitbea@users.noreply.github.com>
2020-08-11 12:44:42 +0530

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,44 +1,78 @@
-var numberRandom1 = Math.floor((Math.random() * 6) + 1);
-var diceImageRandom1 = "images/dice" + numberRandom1 + ".png";
-var image1 = document.querySelectorAll("img")[0];
+// alert("JAVASCRIPT loaded!");
 
-image1.setAttribute("src", diceImageRandom1);
+// DECLARATION OF VARIABLES
 
-// IMAGE 2 AREA
+var i, j = document.querySelectorAll(".drum").length;
+var buttonInnerHtml;
 
-var numberRandom2 = Math.floor((Math.random() * 6) + 1);
-var diceImageRandom2 = "images/dice" + numberRandom2 + ".png";
-var image2 = document.querySelectorAll("img")[1];
+// DETECTING VIA BUTTONS ON BROWSER WINDOW
 
-image2.setAttribute("src", diceImageRandom2);
+for (i = 0; i < j; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    buttonInnerHtml = this.innerHTML;
+    sound(buttonInnerHtml);//to play sounds
+    buttonAnimation(buttonInnerHtml);//to play animation
+  })
+}
 
-// ALTERNATE LOGIC
+// DETECTING VIA KEYBOARD PRESS
 
-// if(numberRandom1 === 1){
-//   image1.setAttribute("src","images/dice1.png");
-// }
-// else if (numberRandom1 === 2) {
-//   image1.setAttribute("src","images/dice2.png");
-// }
-// else if (numberRandom1 === 3) {
-//   image1.setAttribute("src","images/dice3.png");
-// }
-// else if (numberRandom1 === 4) {
-//   image1.setAttribute("src","images/dice4.png");
-// }
-// else if (numberRandom1 === 5) {
-//   image1.setAttribute("src","images/dice5.png");
-// }
-// else {
-//   image1.setAttribute("src","images/dice6.png");
-// }
+document.addEventListener("keypress", function (event){
+  sound(event.key);//to play sounds
+  buttonAnimation(event.key);//to play animation
+});
 
-// WINNERS SECTION
-var displayMessage = document.querySelectorAll("h1")[0];
-if (numberRandom1 === numberRandom2) {
-  displayMessage.innerHTML = "Draw!";
-} else if (numberRandom1 > numberRandom2) {
-  displayMessage.innerHTML = "Player 1 Wins!";
-} else {
-  displayMessage.innerHTML = "Player 2 Wins!";
+
+// CORE OF SOUNDS
+
+function sound(key){
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+
+      break;
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+
+      break;
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+
+      break;
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+
+      break;
+    case "l":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+
+      break;
+    default: console.log(buttonInnerHtml);
+
+  }
+}
+
+// BUTTON ANIMATION
+
+function buttonAnimation (currentKey){
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");//to remove button pressed effect
+  }, 100)
 }
